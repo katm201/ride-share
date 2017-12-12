7e1da7336c1d8af90ef66b3d900300acd96946f1
Katherine Manning <katm201@gmail.com>
2017-12-11 19:32:17 -0800

    (test) update travis config to auto deploy only on successful builds of master

--- service/src/server/routes.js
+++ service/src/server/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (request, response) => {
-  response.status(200).end('Hello Katherine');
+  response.status(200).end('Hello Katherine!');
 });
 
 export default router;
