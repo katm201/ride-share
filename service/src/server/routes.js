deab0640c4d3463a2e553d638fde792c79bb2aa9
Katherine Manning <katm201@gmail.com>
2017-12-11 19:16:07 -0800

    (fix) update docker config on travis

--- service/src/server/routes.js
+++ service/src/server/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (request, response) => {
-  response.status(200).end('Hello');
+  response.status(200).end('Hello world');
 });
 
 export default router;
