2bb66b4cdcdf3dbf34eae457f70b71a9911f6a0b
Katherine Manning <katm201@gmail.com>
2017-12-12 19:32:25 -0800

    (test) add test for inserting a driver

---
+++ test/dbSpec.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+
+const expect = require('chai').expect;
+
+require('dotenv').config();
+
+const db = require('../service/build/database/index.js');
+const tables = require('../service/build/database/config.js');
+
+const { pgKnex } = db.default;
+const { Driver } = tables.default;
+
+describe('drivers table', () => {
+  const tester = { name: 'Johnny Tester' };
+
+  afterEach(() => {
+    pgKnex.destroy();
+  })
+
+  it('Driver model can be used to insert into the database', (done) => {
+    Driver.forge(tester).save().then((response) => {
+      expect(response.attributes.name).to.equal(tester.name);
+
+      return response.destroy();
+    })
+    .then((response) => {
+      done();
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  });
+});
+
+/* eslint-enable */
