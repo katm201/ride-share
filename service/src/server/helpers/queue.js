2af26661ea81f6f68963faa0d47c533d07e5ebbb
Katherine Manning <katm201@gmail.com>
2017-12-16 09:47:01 -0800

    (feat) add ride processing to queue

--- service/src/server/helpers/queue.js
+++ service/src/server/helpers/queue.js
@@ -1,10 +1,35 @@
+import db from '../../database/index';
 import service from '../index';
 
+const { pgKnex } = db;
+
+const getNearestDrivers = job => (
+  pgKnex('drivers').select('*').orderByRaw(`ST_Distance(location, ST_GeometryFromText('${job.start_loc}', 4326)) DESC LIMIT 5`).where({ booked: false, available: true })
+);
+
+const updateDrivers = (drivers) => {
+  const driverUpdates = drivers.map(driver => (pgKnex('drivers').where('id', driver.id).update({ booked: true })));
+  return Promise.all(driverUpdates);
+};
+
+const processQueue = {
+  rides: () => (
+    service.queue.process('ride', 1, (job, done) => {
+      getNearestDrivers(job.data)
+        .then(drivers => (updateDrivers(drivers)))
+        .then((response) => {
+          console.log(response);
+          done();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+  ),
+};
+
 const checkQueue = () => {
-  service.queue.inactive((err, ids) => {
-    if (err) { console.log(err); }
-    console.log(ids);
-  });
+  processQueue.rides();
 };
 
 const helpers = {
--- service/src/server/index.js
+++ service/src/server/index.js
@@ -2,9 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import kue from 'kue';
 import dotenv from 'dotenv';
+import events from 'events';
 
 import router from './routes';
 import helpers from './helpers/queue';
+
+events.EventEmitter.prototype._maxListeners = 0;
 
 dotenv.config();
 
@@ -18,7 +21,7 @@ server.use('/', router);
 
 const { checkQueue } = helpers;
 
-setInterval(() => { checkQueue(); }, 10000);
+setInterval(() => { checkQueue(); }, 1000);
 
 const port = process.env.PORT || 80;
 
