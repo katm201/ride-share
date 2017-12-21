0b643cd724e61c0d7d884c5545d53834f9fbe85b
Katherine Manning <katm201@gmail.com>
2017-12-21 11:42:44 -0800

    (ref) update ping test

--- load-test/ping.js
+++ load-test/ping.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const axios = require('axios');
+const prompt = require('prompt');
 
 // const url = 'http://localhost:3333';
-const url = process.env.EC2_URL;
+// const url = process.env.EC2_URL;
 
-const pingTest = (totalRequests) => {
+const pingTest = (totalRequests, url) => {
   for (let i = 0; i < totalRequests; i++) {
     axios.get(url)
       .then(() => {})
@@ -14,4 +15,13 @@ const pingTest = (totalRequests) => {
   }
 };
 
-pingTest(10);
+// pingTest(10);
+
+prompt.start();
+
+prompt.get(['totalPings', 'toLocal'], (err, result) => {
+  console.log(`Input recieved: sending ${result.totalPings} to ${result.toLocal === 'true' ? 'local /' : 'deployed /'}`);
+  const baseUrl = result.toLocal === 'true' ? `http://localhost:${process.env.PORT}` : process.env.EC2_URL;
+  pingTest(result.totalPings, `${baseUrl}/`);
+});
+
