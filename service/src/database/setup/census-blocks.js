fdbce4117327793e00093cd78e8201cf2f13f245
Katherine Manning <katm201@gmail.com>
2017-12-29 12:00:35 -0800

    (bug) format census-blocks before batch inserting

--- service/src/database/setup/census-blocks.js
+++ service/src/database/setup/census-blocks.js
@@ -10,15 +10,45 @@ const pgKnex = knex({
   pool: { min: 0, max: 10 },
 });
 
+const formatData = (data) => {
+  const queries = data.split(';');
+  const valueStrings = queries.map((query) => {
+    const values = query.slice(query.indexOf(')') + 12, -3);
+    return values;
+  });
+  const inserts = valueStrings.slice(0, -1).map((string) => {
+    const values = string.split('\',\'');
+    const info = {
+      aland10: parseInt(values[0], 10),
+      awater10: parseInt(values[1], 10),
+      countyfp10: values[2],
+      tractce10: values[3],
+      mtfcc10: values[4],
+      name10: values[5],
+      namelsad10: values[6],
+      intptlon10: values[7],
+      funcstat10: values[8],
+      intptlat10: values[9],
+      statefp10: values[10],
+      geoid10: values[11],
+      geom: values[12],
+    };
+    return info;
+  });
+  return inserts;
+};
+
 fs.readFile(path.join(__dirname, 'shapefile.sql'), 'utf8', (err, data) => {
-  pgKnex.raw(data)
-    .then(() => {
+  const inserts = formatData(data);
+
+  pgKnex.batchInsert('census_blocks', inserts, 1000)
+    .then((response) => {
+      console.log(response);
       console.log('complete');
-      return pgKnex.destroy();
+      pgKnex.destroy();
     })
     .catch((err) => {
       console.log(err);
-    })
+    });
 });
-
 
