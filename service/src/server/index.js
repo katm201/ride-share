6aaa7a075d5436571cfb6162aad764ac6bd7dfa0
Katherine Manning <katm201@gmail.com>
2018-01-18 14:28:57 -0800

    (doc) add comments to server/index.js

--- service/src/server/index.js
+++ service/src/server/index.js
@@ -12,12 +12,15 @@ const { pollSQS } = require('./helpers/receive-sqs');
 const sendMetrics = require('./helpers/send-sqs');
 const processQueue = require('./helpers/process-queue');
 
+// solves error from the setIntervals below 
 events.EventEmitter.prototype._maxListeners = 0;
 
 module.exports.server = express();
 
 const { server } = module.exports;
 
+// creates a queue for the incoming
+// POST requests to /new_ride
 module.exports.queue = kue.createQueue({
   redis: {
     port: process.env.KUE_REDIS_PORT,
@@ -25,6 +28,8 @@ module.exports.queue = kue.createQueue({
   },
 });
 
+// sets up connection to AWS SQS to use
+// as a message bus
 module.exports.sqs = new AWS.SQS({
   region: 'us-east-2',
   maxRetries: 15,
@@ -37,14 +42,19 @@ module.exports.sqs = new AWS.SQS({
 
 server.use(bodyParser.json());
 
+// sends HTTP requests over to the ./routes.js file
 server.use('/', router);
 
 const kuePollInterval = process.env.KUE_POLL_INTERVAL || 100;
 const sqsPollInterval = process.env.SQS_POLL_INTERVAL || 1000;
 const metricsInterval = process.env.METRICS_INTERVAL || 300000;
 
+// runs background transactions for:
+// polling SQS
 setInterval(() => { pollSQS(); }, sqsPollInterval);
+// sending metrics to metrics reporting service
 setInterval(() => { sendMetrics(); }, metricsInterval);
+// processing the new ride queue
 setInterval(() => { processQueue.processRides(); }, kuePollInterval);
 
 const port = process.env.PORT || 80;
