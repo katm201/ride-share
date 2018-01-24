cb3900d3d0242186fe0259580d302e0d082c5976
Katherine Manning <katm201@gmail.com>
2018-01-23 16:05:34 -0800

    (doc) add comments to census-blocks seeding script

--- service/src/database/setup/census-blocks.js
+++ service/src/database/setup/census-blocks.js
@@ -10,6 +10,9 @@ const pgKnex = knex({
   pool: { min: 0, max: 10 },
 });
 
+// script to pipe data from the SHAPEFILE.sql file into PostgreSQL via Knex
+// built for seeding the deployed database, since the Node docker container
+// doesn't come with psql
 fs.readFile(path.join(__dirname, 'SHAPEFILE.sql'), 'utf8', (err, data) => {
   pgKnex.raw(data)
     .then((response) => {
